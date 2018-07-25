6d578691e7c8318602d2bb31751827c5a6ea8a00
Andres Echeverria <andreseg@umich.edu>
2018-07-25 14:23:55 -0400

    Removed unnecessary comments

--- PythonClient/computer_vision/cv_mode.py
+++ PythonClient/computer_vision/cv_mode.py
@@ -9,7 +9,9 @@ import os
 import time
 
 import json
+import math
 
+# Class that keeps the position and orientation of a vehicle, use radians.
 class cameraPose():
     def __init__(self, x, y, z, yaw, pitch, roll):
         self.x = x
@@ -19,11 +21,12 @@ class cameraPose():
         self.pitch = pitch
         self.roll = roll
 
-
+# Reading from the json file
 with open('rendering_modeling_v1887_new.json') as file:
     data = json.load(file)
 
-#Finding out how many vehicles there are in this json
+# Finding out how many vehicles there are in this json and creating an empty 
+# 2D array with that many spaces
 numvehicles = 0
 
 routes = []
@@ -34,15 +37,15 @@ for vehicle in data:
     else:
          break  
          
+# Reading in the poses and filling the array         
 for vehicle in data:
     pose = cameraPose(float(vehicle["center_coords"]["center_x"]), 
                float(vehicle["center_coords"]["center_y"]), 
                float(vehicle["center_coords"]["center_z"]),
-               float(vehicle["euler_angles"]["yaw"]),
-               float(vehicle["euler_angles"]["pitch"]),
-               float(vehicle["euler_angles"]["roll"]))
+               math.radians(float(vehicle["euler_angles"]["yaw"])),
+               math.radians(float(vehicle["euler_angles"]["pitch"])),
+               math.radians(float(vehicle["euler_angles"]["roll"])))
 
-    #print(int(vehicle["id"]) - 1)
     routes[int(vehicle["id"]) - 1].append(pose)
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -50,6 +53,8 @@ pp = pprint.PrettyPrinter(indent=4)
 client = airsim.VehicleClient()
 client.confirmConnection()
 
+# This loop will set the camera pose to the ones in the 2D array. Currently 
+# only works for one vehicle
 airsim.wait_key('Press any key to move')
 for index, waypoint in enumerate(routes[0]):
     client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(routes[0][index].x, 
@@ -57,47 +62,13 @@ for index, waypoint in enumerate(routes[0]):
                                                          routes[0][index].z), 
                              airsim.to_quaternion(routes[0][index].pitch, 
                                                   routes[0][index].roll, 
-                                                  routes[0][index].yaw)), False) 
-    #print("Yaw: %f, Pitch: %f, Roll: %f" % (routes[0][index].yaw, routes[0][index].pitch, routes[0][index].roll))                                              
+                                                  routes[0][index].yaw)), True) 
     print("Waypoint number: %d" % index)
+
+    # Printing the pose for debugging purposes.
     pose = client.simGetVehiclePose()
     pp.pprint(pose)
-    time.sleep(3)   
-
-
-airsim.wait_key('Press any key to set camera-0 gimble to 15-degree pitch')
-#client.simSetCameraOrientation("0", airsim.to_quaternion(0.261799, 0, 0)) #radians
-
-airsim.wait_key('Press any key to get camera parameters')
-#for camera_name in range(5):
-#    camera_info = client.simGetCameraInfo(str(camera_name))
-#    print("CameraInfo %d: %s" % (camera_name, pp.pprint(camera_info)))
-
-#airsim.wait_key('Press any key to get images')
-#for x in range(3): # do few times
-#    z = x * -20 - 5 # some random number
-#    client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(z, z, z), airsim.to_quaternion(x / 3.0, 0, x / 3.0)), True)
-
-#     responses = client.simGetImages([
-#         airsim.ImageRequest("0", airsim.ImageType.DepthVis),
-#         airsim.ImageRequest("1", airsim.ImageType.DepthPerspective, True),
-#         airsim.ImageRequest("2", airsim.ImageType.Segmentation),
-#         airsim.ImageRequest("3", airsim.ImageType.Scene),
-#         airsim.ImageRequest("4", airsim.ImageType.DisparityNormalized),
-#         airsim.ImageRequest("4", airsim.ImageType.SurfaceNormals)])
-
-#     for i, response in enumerate(responses):
-#         if response.pixels_as_float:
-#             print("Type %d, size %d, pos %s" % (response.image_type, len(response.image_data_float), pprint.pformat(response.camera_position)))
-#             airsim.write_pfm(os.path.normpath('temp/cv_mode_' + str(x) + "_" + str(i) + '.pfm'), airsim.get_pfm_array(response))
-#         else:
-#             print("Type %d, size %d, pos %s" % (response.image_type, len(response.image_data_uint8), pprint.pformat(response.camera_position)))
-#             airsim.write_file(os.path.normpath('temp/cv_mode_' + str(x) + "_" + str(i) + '.png'), response.image_data_uint8)
-
-#     pose = client.simGetVehiclePose()
-#     pp.pprint(pose)
-
-#     time.sleep(3)
-
+    time.sleep(3)
+    
 #currently reset() doesn't work in CV mode. Below is the workaround
 client.simSetPose(airsim.Pose(airsim.Vector3r(0, 0, 0), airsim.to_quaternion(0, 0, 0)), True)
